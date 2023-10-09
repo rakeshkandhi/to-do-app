728b11fbf4cb4fcb36870588478d10154c969b22
rakeshkandhi <rakeshkandhi1432@gmail.com>
2023-10-09 14:29:20 +0530

    init

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Note from './Note';
 import Addnote from '../CreateNote';
 // import axios from 'axios';
-
 
 // code for fetching data from endpoint using async and await
 export default function Notes() {
@@ -19,39 +17,37 @@ export default function Notes() {
 
     async function saveNote(payload) {
         const res = await fetch('/todo', {
-            method: "POST",
+            method: 'POST',
             body: JSON.stringify(payload),
             headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            }
+                'Content-Type': 'application/json; charset=utf-8',
+            },
         });
         const data = await res.json();
         console.log(`Saved note: ${JSON.stringify(data)}`);
         getAllNotes();
     }
 
-
     async function deleteNote(id) {
         const res = await fetch(`/todo/${id}`, {
-            method: "DELETE",
+            method: 'DELETE',
             headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            }
-        })
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        });
         const data = await res.json();
         console.log(`deletd note: ${JSON.stringify(data)}`);
         getAllNotes();
     }
 
-
     async function updateNote(id, payload) {
         await fetch(`/todo/${id}`, {
-            method: "PUT",
+            method: 'PUT',
             body: JSON.stringify(payload),
             headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            }
-        })
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        });
         getAllNotes();
     }
     // code for fetching data from endpoint using only useEffect
@@ -60,8 +56,6 @@ export default function Notes() {
     //         .then(res => res.json())
     //         .then(data => setNotes(data)).catch(error => console.log(error));
     // },[])
-
-
 
     // code for fetching data from endpoint using axios() and async and await)
     // useEffect(() => {
@@ -72,37 +66,39 @@ export default function Notes() {
     //         }
     //         catch(error){
     //             console.log(error);
-    //         }   
+    //         }
 
     //     }
     //     getAllNotes();
     // },[])
-
 
     return (
         <div className='container my-3'>
             <h1>Take your notes</h1>
             <Addnote saveNoteFun={saveNote} />
 
-
             <hr />
             <h1>Your Notes</h1>
             <hr />
 
-
             <div id='notes' className='row conitainer-fluid'>
                 {notes.length === 0 ? (
-                    <span className='d-block p-2 text-bg-dark'>No saved notes</span>
+                    <span className='d-block p-2 text-bg-dark'>
+                        No saved notes
+                    </span>
                 ) : (
                     notes.map((note, index) => {
-                        return <Note
-                         noteItem={note}
-                          index={index}
-                           deleteFun={deleteNote} 
-                           updateFun={updateNote} />
-                    }
-                    ))}
+                        return (
+                            <Note
+                                noteItem={note}
+                                index={index}
+                                deleteFun={deleteNote}
+                                updateFun={updateNote}
+                            />
+                        );
+                    })
+                )}
             </div>
         </div>
     );
-};
+}
